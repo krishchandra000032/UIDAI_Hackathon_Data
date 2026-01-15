d89c60d560f3eef1dd1c5320106bc35d1833f36f
Krish Chandra <krishuchandra1783@gmail.com>
2026-01-15 17:47:24 +0530

    Update merger_data.py

--- merger_data.py
+++ merger_data.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import glob
 import os
-
-# --- Configuration ---
 INPUT_FOLDER = "clean_data"
 OUTPUT_FILE = "final_master_dataset.csv"
 
 
 def merge_clean_files():
-    # Find all cleaned CSV files
     search_path = os.path.join(INPUT_FOLDER, "clean_*.csv")
     files = glob.glob(search_path)
 
@@ -24,9 +21,6 @@ def merge_clean_files():
         try:
             filename = os.path.basename(file_path)
             df = pd.read_csv(file_path)
-
-            # --- Auto-Tagging Categories ---
-            # This helps you filter the master file later
             if "biometric" in filename.lower():
                 df['data_category'] = 'Biometric Update'
             elif "demographic" in filename.lower():
@@ -35,8 +29,6 @@ def merge_clean_files():
                 df['data_category'] = 'New Enrolment'
             else:
                 df['data_category'] = 'Other'
-
-            # Add source filename for traceability
             df['source_file'] = filename
 
             data_frames.append(df)
@@ -45,16 +37,12 @@ def merge_clean_files():
         except Exception as e:
             print(f"  Error reading {filename}: {e}")
 
-    # --- Concatenate ---
-    # sort=False prevents warning about column order
     master_df = pd.concat(data_frames, ignore_index=True, sort=False)
-
-    # Fill NaN values with 0 for numerical columns (Optional but recommended for analysis)
-    # This ensures that a 'Biometric' row has '0' for 'New Enrolment' counts instead of empty space.
+    
     numeric_cols = master_df.select_dtypes(include=['number']).columns
+    
     master_df[numeric_cols] = master_df[numeric_cols].fillna(0)
 
-    # Save
     master_df.to_csv(OUTPUT_FILE, index=False)
 
     print("-" * 30)
